09b3773e8592747a13d38827fac72698ff2654bd
DSP <dsp771122@gmail.com>
2025-05-12 14:44:43 +0530

    final

--- server/server.js
+++ server/server.js
@@ -1,41 +1,46 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const cors=require('cors');
-const dotenv=require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 const authRoutes = require('./routes/authRoute');
-const serviceRoutes=require('./routes/serviceRoute');
-const searchRoute=require('./routes/searchRoute');
-const vendorRoutes=require('./routes/vendorRoute');
-const cartRoute=require('./routes/cartRoutes');
-const bookingRoute=require('./routes/bookingsRoutes');
-const detailRoute=require('./routes/detailRoute');
-const reviewRoutes=require('./routes/reviewRoute');
-const fullRoute=require('./routes/fullRoute');
-const db=require('./config/connectDB');
-const app=express();
+const serviceRoutes = require('./routes/serviceRoute');
+const searchRoute = require('./routes/searchRoute');
+const vendorRoutes = require('./routes/vendorRoute');
+const cartRoute = require('./routes/cartRoutes');
+const bookingRoute = require('./routes/bookingsRoutes');
+const detailRoute = require('./routes/detailRoute');
+const reviewRoutes = require('./routes/reviewRoute');
+const fullRoute = require('./routes/fullRoute');
+const db = require('./config/connectDB');
+const app = express();
 
+// CORS middleware
 app.use(cors({
-  origin:[ 'http://localhost:5173','https://swiftserve-4v01.onrender.com'],
+  origin: ['http://localhost:5173', 'https://swiftserve-4v01.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  allowedHeaders: ['Content-Type', 'Authorization', '*'], // Allow all headers
+  credentials: true,
 }));
+
+app.options('*', cors()); // Handle OPTIONS requests for all routes
 
 app.use(express.json());
 dotenv.config();
 db();
 
+// Routes
 app.use('/auth', authRoutes);
-app.use('/service',serviceRoutes);
+app.use('/service', serviceRoutes);
 app.use('/search', searchRoute);
-app.use('/vendor',vendorRoutes);
-app.use('/review',reviewRoutes); 
-app.use('/cart',cartRoute); 
-app.use('/bookings',bookingRoute);
-app.use('/detail',detailRoute);
-app.use('/vendor-service',fullRoute);
+app.use('/vendor', vendorRoutes);
+app.use('/review', reviewRoutes);
+app.use('/cart', cartRoute);
+app.use('/bookings', bookingRoute);
+app.use('/detail', detailRoute);
+app.use('/vendor-service', fullRoute);
 
-app.listen(process.env.PORT,()=>{
-    console.log("Server is running on");
-})
+// Start server
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port " + process.env.PORT);
+});
